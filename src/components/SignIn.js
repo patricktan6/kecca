6c3aac5dfe443e01b4032c6b820b2e298e55f9de
Lye Yi Xian <lyeyx999@hotmail.com>
2020-06-21 12:26:14 +0800

    added form tag on Sign In page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -28,45 +28,47 @@ const SignIn = ({ history }) => {
   }
   // I don't know where to put the onSubmit handler need your help!!
   return (
-    <div className="bg">
-      <div className="login-box">
-        <h1>Login</h1>
-        <div className="textbox">
-          <svg
-            class="bi bi-person-fill"
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-            />
-          </svg>
-          <input type="text" placeholder="Email" />
+    <form onSubmit={handleSignIn}>
+      <div className="bg">
+        <div className="login-box">
+          <h1>Login</h1>
+          <div className="textbox">
+            <svg
+              class="bi bi-person-fill"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="textbox">
+            <svg
+              class="bi bi-lock-fill"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="11" height="9" x="2.5" y="7" rx="2" />
+              <path
+                fill-rule="evenodd"
+                d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
+              />
+            </svg>
+            <input type="password" placeholder="Password" />
+          </div>
+          <input className="btn" type="button" value="Sign In" />
         </div>
-        <div className="textbox">
-          <svg
-            class="bi bi-lock-fill"
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="11" height="9" x="2.5" y="7" rx="2" />
-            <path
-              fill-rule="evenodd"
-              d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
-            />
-          </svg>
-          <input type="password" placeholder="Password" />
-        </div>
-        <input className="btn" type="button" value="Sign In" />
       </div>
-    </div>
+    </form>
   );
 };
 
