6c966e0316390a013c54c3d71f6b895b1bb41b03
Lye Yi Xian <lyeyx999@hotmail.com>
2020-06-29 23:24:19 +0800

    Added Event to the page

--- src/pages/Admin/Request.js
+++ src/pages/Admin/Request.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 export class Request extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Request</h1>
+      </div>
+    );
   }
 }
 
