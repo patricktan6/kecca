678ca4ca1fffbd31166b1e6849233bfa798ba8de
Lye Yi Xian <lyeyx999@hotmail.com>
2020-06-29 18:00:25 +0800

    Fixed refreshing to a blank page bug on admin page

--- src/pages/Admin/Navigation.js
+++ src/pages/Admin/Navigation.js
@@ -4,40 +4,38 @@ import { NavLink } from "react-router-dom";
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand navbar-light bg-light">
         <NavLink className="navbar-brand" to="/admin">
           KECCA
         </NavLink>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin/home">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin/attendance">
-                Attendance Taking
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin/request">
-                Request
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin/members">
-                Members Info
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin/contact">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/admin/home">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/admin/attendance">
+              Attendance Taking
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/admin/request">
+              Request
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/admin/members">
+              Members Info
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/admin/contact">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     );
   }
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -8,18 +8,16 @@ import MembersInfo from "./MembersInfo";
 import Contact from "./Contact";
 
 const Admin = () => {
-  console.log("hi");
   return (
     <Router>
       <div>
         <Navigation />
-        <Switch>
-          <Route path="/admin/home" component={Home} exact />
-          <Route path="/admin/attendance" component={AttendanceMarking} />
-          <Route path="/admin/request" component={Request} />
-          <Route path="/admin/members" component={MembersInfo} />
-          <Route path="/admin/contact" component={Contact} />
-        </Switch>
+
+        <Route path="/admin/home" component={Home} />
+        <Route path="/admin/attendance" component={AttendanceMarking} />
+        <Route path="/admin/request" component={Request} />
+        <Route path="/admin/members" component={MembersInfo} />
+        <Route path="/admin/contact" component={Contact} />
       </div>
     </Router>
   );
