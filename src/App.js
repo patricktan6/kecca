870cfc56b95f8780caa5b36dfcf95fcf9ba668a0
Lye Yi Xian <lyeyx999@hotmail.com>
2020-06-29 21:50:51 +0800

    Fixed App.js (move to the correct line)

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       <Router>
         <div>
           <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/admin/*" component={Admin} />
           <Route exact path="/admin" component={Admin} />
+          <Route exact path="/admin/*" component={Admin} />
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/signup" component={SignUp} />
         </div>
--- src/pages/Admin/AttendanceMarking.js
+++ src/pages/Admin/AttendanceMarking.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
-const AttendanceMarking = () => {
-  return (
-    <div>
-      <h1>Attendance Marking</h1>
-    </div>
-  );
-};
+class AttendanceMarking extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Attendance Marking</h1>
+      </div>
+    );
+  }
+}
 
 export default AttendanceMarking;
